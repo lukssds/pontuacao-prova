676da438ed7267fd3331374af6a6c1b1e4a3f2c7
lukssds <lukssds@gmail.com>
2020-05-15 19:17:05 -0300

    Cabecalho

--- src/pontuacaoprova/exerciciopontuacao.java
+++ src/pontuacaoprova/exerciciopontuacao.java
@@ -1,10 +1,20 @@
 package pontuacaoprova;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class exerciciopontuacao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+		System.out.println("");
+		
 	}
 
 }
