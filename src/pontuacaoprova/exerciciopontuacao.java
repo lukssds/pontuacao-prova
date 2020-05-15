b5750741f0b7be71cbba4f3b2c1262ecc9518217
lukssds <lukssds@gmail.com>
2020-05-15 19:23:26 -0300

    Leitura do volume

--- src/pontuacaoprova/exerciciopontuacao.java
+++ src/pontuacaoprova/exerciciopontuacao.java
@@ -10,10 +10,18 @@ public class exerciciopontuacao {
 		Locale.setDefault(Locale.US);
 		Scanner sc= new Scanner(System.in);
 		
+		double volumedecompras,ticketmedio;
+		
+		//cabeçalho
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		System.out.println("");
+		//leitura de compras
+		System.out.print("Quantas compras o cliente fez no último ano? ");
+		volumedecompras=sc.nextDouble();
+		System.out.print("Qual o ticket médio? ");
+		ticketmedio=sc.nextDouble();
 		
 	}
 
