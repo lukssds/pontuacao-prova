20005408bb5f2628a999f0a13302c689ce88eca7
lukssds <lukssds@gmail.com>
2020-05-15 19:29:26 -0300

    Leitura dos outros dados

--- src/pontuacaoprova/exerciciopontuacao.java
+++ src/pontuacaoprova/exerciciopontuacao.java
@@ -11,17 +11,30 @@ public class exerciciopontuacao {
 		Scanner sc= new Scanner(System.in);
 		
 		double volumedecompras,ticketmedio;
+		int atrasopagamento;
+		char dcb;
 		
 		//cabeçalho
+		
 		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
 		System.out.println("--------------------------------");
 		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
 		System.out.println("");
-		//leitura de compras
+		
+		//leitura de volume
+		
 		System.out.print("Quantas compras o cliente fez no último ano? ");
 		volumedecompras=sc.nextDouble();
 		System.out.print("Qual o ticket médio? ");
 		ticketmedio=sc.nextDouble();
+		
+		//leitura dos outros dados
+		
+		System.out.println("");
+		System.out.print("Quantas vezes o cliente atrasou o pagamento? ");
+		atrasopagamento=sc.nextInt();
+		System.out.print("A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ");
+		dcb=sc.next().charAt(0);
 		
 	}
 
